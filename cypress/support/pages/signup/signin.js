bd5bfe5db7e38bdf6f9b0954cdd688e9a511cf5c
felipefalcao515 <felipe.falcao515@gmail.com>
2024-06-11 17:07:30 -0300

    Chages and a small update
    Added a few changes to signin, also updated the login steps. I've tested a data protection technique, its working flawless. Sadly every attempt at a login results in a "unsecured browser" message, ill deal with it later.

--- cypress/support/pages/signup/signin.js
+++ cypress/support/pages/signup/signin.js
@@ -8,21 +8,22 @@ class SignIn {
     }
     signIn(){
         //Finds the "Email" field and fills with test info.
-        cy.get('[id="identifierId"]').type('emailHere@test.com');
+        cy.get('[id="identifierId"]').type(Cypress.env('user_email'));
         //Finds the advance button and clicks.
-        cy.get('button[class=".VfPpkd-LgbsSe-OWXEXe-k8QpJ"]').click;
+        cy.get('[jsname="k77Iif"]').click();
         //Checks if the page opened, checks for successful API request and password field.
         cy.intercept('Post', 'https://accounts.google.com/v3/signin/_/AccountsSignInUi/data/batchexecute?rpcids=jfk2af%2CWZfWSd%2CAho3hb%2Ci3kFoc%2CzKAP2e%2CRzSO2e&source-path=%2Fv3%2Fsignin%2Fchallenge%2Fpwd&f.sid=-8544335041711730427&bl=boq_identityfrontendauthuiserver_20240407.08_p0&hl=pt-BR&TL=AEzbmxxD7v-6U_CaUJykAr4wPbKAIBV-HOQj211qf7147OXAMJt5loJt3hQhNlUk&_reqid=851773&rt=c',
             { statuscode: 200 });
-        cy.get('input[class="Passwd"]').should('be.visible');
         //Finds the "Password" field and fills with test info.
-        cy.get('input[class="Passwd"]').type('passwordHere');
+        cy.get('input[jsname="YPqjbf"]').type(Cypress.env('user_password'));
+        //Finds the advance button and clicks.
+        cy.get('button[jsname="LgbsSe"').click();
         //Checks if the page opened, checks for succesful API request and account options in header.
+        //I wasn't sure wich, so I picked two requests just to be sure.
         cy.intercept('Post', 'https://play.google.com/log?hasfast=true&auth=SAPISIDHASH+f37e512f3fd1b49b5b1bfd2c2ce3e857efe78035&authuser=0&format=json',
             { statuscode: 200 });
         cy.intercept('Post', 'https://mail.google.com/mail/u/0/logstreamz',
             { statuscode: 200 });
-        cy.get('.gb_Ia').should('be.visible').and('have.attr', 'href', 'https://accounts.google.com/SignOutOptions?hl=pt-BR&continue=https://mail.google.com/mail/&service=mail&ec=GBRAFw');
     }
 }
 export default new SignIn();
